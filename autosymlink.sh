b6ab0d7e4abee427a15c2c38923d30f3d39ef6f5
HumanoidSandvichDispenser <troll1.lolzz@gmail.com>
2019-10-13 20:35:17 -0700

    fixed folders symlink recursion

---
+++ autosymlink.sh
@@ -0,0 +1,9 @@
+# This script automatically generates symlinks from the home directory to this directory
+
+dir="$HOME/git/dotfiles"
+files=".bashrc .zshrc .vimrc"
+
+for file in $files; do
+	echo "Creating symlink for $file"
+	ln -s $dir/$file ~/$file
+done
