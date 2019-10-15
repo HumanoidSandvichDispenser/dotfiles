0f269bb43b48a9478326ed4e9c4d598896759a35
HumanoidSandvichDispenser <troll1.lolzz@gmail.com>
2019-10-14 21:56:05 -0700

    image to black and white binary converter

---
+++ bin/bwbinary.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import math
+import click
+from PIL import Image
+
+def GetValue(r, g, b):
+    return max(r, max(g, b))
+
+@click.command()
+@click.argument("file")
+@click.argument("destination")
+def Main(file, destination):
+    image = Image.open(file).convert("RGB")
+    picture = image.load()
+    width, height = image.size
+    text = ""
+
+    # Loop through each pixel of the image
+    for y in range(width):
+        for x in range(height):
+            # Set r, g, b to increase readability
+            r, g, b = picture[x, y][0], picture[x, y][1], picture[x, y][2],  
+            
+            # Get lightness value given r, g, b
+            # If the lightness value is less than 50%, the pixel is 0 (otherwise 1)
+            value = GetValue(r, g, b)
+            if (value < 128):
+                text += '0'
+            else:
+                text += '1'
+        text += '\n'
+
+    destinationFile = open(destination, "w+")
+    destinationFile.write(binary)
+    destinationFile.close()
+
+if __name__ == "__main__":
+    Main()
