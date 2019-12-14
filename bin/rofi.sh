292c399f85bc7130d41612d29f56f354d1b92d23
HumanoidSandvichDispenser <troll1.lolzz@gmail.com>
2019-12-13 16:47:21 -0800

    added rofi configuration

---
+++ bin/rofi.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# rofi.sh
+# Copyright (C) 2019 sandvich <sandvich@sandvich-pc>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+source /etc/locale.conf
+rofi -combi-modi drun,window -show combi -modi combi.drun.window -show-icons -icon-theme Zafiro-Icons-Black-Red
