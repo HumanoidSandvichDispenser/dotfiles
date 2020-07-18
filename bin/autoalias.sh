ae83795e5096f898e04109fee200eb765facc065
HumanoidSandvichDispenser <troll1.lolzz@gmail.com>
2020-07-18 03:05:28 -0700

    removed old files

--- bin/autoalias.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-for file in `ls $HOME/bin/autoalias/`; do
-	alias $file="source $HOME/bin/autoalias/$file"
-	echo "Aliased $HOME/bin/autoalias/$file as $file"
-done
--- bin/autoalias/editalias.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# addalias.sh
-# Copyright (C) 2019 sandvich <sandvich@sandvich-pc>
-#
-# Distributed under terms of the MIT license.
-#
-
-vim ~/bin/autoalias/$1.sh
