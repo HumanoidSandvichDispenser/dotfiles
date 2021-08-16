f72cd4a6dd1d2b853f92d526eb87ded36d6514c2
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2021-08-15 21:33:08 -0700

    autosymlink retab and new config

--- autosymlink.sh
+++ autosymlink.sh
@@ -4,25 +4,25 @@
 currDir="$HOME/git/dotfiles"
 cd $currDir
 declare -a files=(".bashrc" ".zshrc" ".vimrc" ".Xresources")
-declare -a directories=(".config/bspwm" ".config/sxhkd" ".config/ncmpcpp")
+declare -a directories=(".config/bspwm" ".config/sxhkd" ".config/ncmpcpp" ".config/polybar-config")
 
 for file in ${files[@]}; do
-	echo $file
-	if [ ! -f "~/$file" ]; then
-		echo "Creating symlink for $file"
-		ln -s $currDir/$file $HOME/$file
-	else
-		echo "Oldifying existing file: $file"
-		mv ~/$file ~/$file.old1
-	fi
+    echo $file
+    if [ ! -f "~/$file" ]; then
+        echo "Creating symlink for $file"
+        ln -s $currDir/$file $HOME/$file
+    else
+        echo "Oldifying existing file: $file"
+        mv ~/$file ~/$file.old1
+    fi
 done
 
 for directory in ${directories[@]}; do
-	if [ ! -f "~/$directory" ]; then
-		echo "Creating symlink for $directory"
-		ln -s $currDir/$directory $HOME/$directory
-	else
-		echo "Oldifying existing directory: $directory"
-		mv ~/$file ~/$file.old1
-	fi
+    if [ ! -f "~/$directory" ]; then
+        echo "Creating symlink for $directory"
+        ln -s $currDir/$directory $HOME/$directory
+    else
+        echo "Oldifying existing directory: $directory"
+        mv ~/$file ~/$file.old1
+    fi
 done
