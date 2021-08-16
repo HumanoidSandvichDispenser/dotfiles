3b8139d867d09002f44b4a7056a923f8f8f9374e
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2021-08-15 21:38:23 -0700

    fixed autosymlink

--- autosymlink.sh
+++ autosymlink.sh
@@ -1,28 +1,32 @@
 #!/usr/bin/zsh
 # This script automatically generates symlinks from the home directory to this directory
 
-currDir="$HOME/git/dotfiles"
+currDir="$DOTFILES"
 cd $currDir
 declare -a files=(".bashrc" ".zshrc" ".vimrc" ".Xresources")
-declare -a directories=(".config/bspwm" ".config/sxhkd" ".config/ncmpcpp")
+declare -a directories=(".config/bspwm" ".config/sxhkd" ".config/ncmpcpp" ".config/nvim")
 
 for file in ${files[@]}; do
 	echo $file
-	if [ ! -f "~/$file" ]; then
+	if [ ! -f "$HOME/$file" ]; then
 		echo "Creating symlink for $file"
 		ln -s $currDir/$file $HOME/$file
 	else
-		echo "Oldifying existing file: $file"
-		mv ~/$file ~/$file.old1
+		if read confirm"?$HOME/$file already exists. Oldify existing file? (y/N)" && [[ $confirm == [yY] ]]; then
+			echo "Oldifying existing file: $file"
+			mv ~/$file ~/$file.old1
+		fi
 	fi
 done
 
 for directory in ${directories[@]}; do
-	if [ ! -f "~/$directory" ]; then
+	if [ ! -d $HOME/$directory ]; then
 		echo "Creating symlink for $directory"
 		ln -s $currDir/$directory $HOME/$directory
 	else
-		echo "Oldifying existing directory: $directory"
-		mv ~/$file ~/$file.old1
+		if read confirm"?$HOME/$directory already exists. Oldify existing directory? (y/N)" && [[ $confirm == [yY] ]]; then
+			echo "Oldifying existing directory: $directory"
+			mv ~/$directory ~/$directory.old1
+		fi
 	fi
 done
