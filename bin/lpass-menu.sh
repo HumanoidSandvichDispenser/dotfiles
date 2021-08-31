849455b062c2e3a3cab674f3841c1234c0ba0fbd
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2021-08-30 21:35:50 -0700

    asdasd

--- bin/lpass-menu.sh
+++ bin/lpass-menu.sh
@@ -1,9 +1,11 @@
-passwords=$(lpass ls --format="<b>%an</b>&#10;%au [%ai]")
+passwords=$(lpass ls --format="<b>%an</b>&#10;%au <span size=\"0\">[%ai]</span>")
 
 [ -n "$passwords" ] && \
     result=$(echo -n "$passwords" |\
     rofi -dmenu -i -markup -markup-rows \
     -theme $DOTFILES/rofi-menu-input-no-icon.rasi -p " lpass" |\
-    sed -E 's/.*\[([0-9]+)\]/\1/gm;t;d')
+    sed -E 's/.*\[([0-9]+)\].*/\1/gm;t;d')
 
 [ -n "$result" ] && lpass show -cp $result
+
+echo $result
