a3da0923a7ba473785f94db8855a6aa3f97345f2
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2021-09-06 19:24:58 -0700

    nvim config and scrot scripts

--- bin/scrot-clip.sh
+++ bin/scrot-clip.sh
@@ -8,22 +8,58 @@
 
 # exit if kdialog is not installed
 # it is required for save file dialogs
-which zenity || (notify-send -a "scrot-clip.sh" "zenity not found"; exit)
+which zenity > /dev/null || (notify-send -a "$(basename $0)" "zenity not found"; exit 1)
 
-filename=$(date +"/tmp/scrot_%Y-%m-%d_%H:%M:%S.png")
+# get options
+while getopts ":f" opt; do
+    case $opt in
+        f)
+            freeze=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+        esac
+done
 
-#scrot -sf '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'
-scrot --select --freeze --line width=2 "$filename" && xclip -selection clipboard -target image/png "$filename" || exit
+if [ -n "$freeze" ]; then
+    maim -u | feh - -xF &
+
+    feh_pid=$!
+
+    i=0
+    while [ -z "$(xdotool search --pid "$feh_pid")" ]; do
+        sleep 0.05
+        [ "$i" -eq "20" ] && (notify-send -a "$(basename $0)" "Could not start feh"; exit 1)
+        echo "waiting for feh..."
+        i=$((i+1))
+    done
+
+    maim -s -b 4 -r invert | xclip -selection clipboard -t image/png
+else
+    maim -s -b 4 --color "1,1,1" -l --color "1,1,1,0.3" | xclip -selection clipboard -t image/png
+fi
+    
+
+
+kill $feh_pid
+# kill feh running in the background when finished
 
 action=$(dunstify --action="save,Save to File" --action="upload,Upload to imgbb" "Copied to clipboard")
 
 case "$action" in
     "save")
-        zenity --file-selection --save | xargs cp $filename
+        xclip -selection clipboard -t image/png > /tmp/clipboard.png
+        zenity --file-selection --save | xargs cp /tmp/clipboard.png
         ;;
     "upload")
         api_key=""
         name=$(rofi -dmenu -p " image name" -theme $DOTFILES/rofi-input-box.rasi)
-        [ -n "$name" ] && [ "$name" != "" ] && imgbb-upload.py $api_key $filename --name $name
+        [ -n "$name" ] && [ "$name" != "" ] && imgbb-upload.py $api_key /tmp/clipboard.png --name $name
         ;;
 esac
