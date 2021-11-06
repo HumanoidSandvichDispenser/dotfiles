ce8779b02b1c34a122756e316530aee8823ffb0d
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2021-11-05 19:06:47 -0700

    lpass auto login

--- bin/lpass-menu.sh
+++ bin/lpass-menu.sh
@@ -1,3 +1,23 @@
+#! /bin/sh
+#
+# lpass-login.sh
+# Copyright (C) 2021 sandvich <sandvich@arch>
+#
+# Distributed under terms of the MIT license.
+#
+# A script to display LastPass passwords using rofi and lpass CLI.
+
+
+# if we are not logged in, login given the secrets.
+if ! lpass status; then
+    notify-send -a "lpass-menu.sh" "Login" "Logging into LastPass..."
+    lpass-login.sh $(xgetres lpass.email) $(xgetres lpass.password) \
+        && notify-send -a "lpass-menu.sh" "Login" "Successfully logged into LastPass"
+fi
+
+# <span size="0"></span> to hide the password ID, but include it so we know what
+# ID of the password is after selecting through rofi. The password ID is required
+# to get the password through the lpass CLI.
 passwords=$(lpass ls --format="<b>%an</b>&#10;%au <span size=\"0\">[%ai]</span>")
 
 [ -n "$passwords" ] && \
