8a39fc31625ef5f46d0011bcf827a29fb288e767
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2022-01-02 00:56:43 -0800

    compatibility with neovim

--- bin/colorscheme.sh
+++ bin/colorscheme.sh
@@ -16,7 +16,7 @@ xrdb -merge $DOTFILES/colorschemes/$1.Xresources
 #    nvr --nostart --servername $path -cc "call SetColorscheme('$1')"
 #done
 
-echo "require('highlight-overrides').SetColorscheme('$1')" > /var/tmp/colorscheme.lua
+echo "autocmd VimEnter * PackerLoad $1" > /var/tmp/colorscheme.vim
 
 # signal st and tabbed to reload config
 kill -USR1 $(pidof st)
@@ -29,6 +29,23 @@ inactive_border=$(get-xresource.sh "background")
 active_border=$(get-xresource.sh "background3")
 focused_border=$(get-xresource.sh "background4")
 
+background=$(xgetres "background")
+background_alt=$(xgetres "background-alt")
+background1=$(xgetres "background1")
+background2=$(xgetres "background2")
+background3=$(xgetres "background3")
+background4=$(xgetres "background4")
+background_dark=$(xgetres "background-dark")
+foreground=$(xgetres "foreground")
+red=$(xgetres "color1")
+green=$(xgetres "color2")
+yellow=$(xgetres "color3")
+blue=$(xgetres "color4")
+magenta=$(xgetres "color5")
+cyan=$(xgetres "color6")
+gray=$(xgetres "color8")
+accent=$(xgetres "accent")
+
 bspc config normal_border_color $inactive_border
 bspc config active_border_color $active_border
 bspc config focused_border_color $focused_border
@@ -37,15 +54,27 @@ feh --bg-scale $WALLPAPERS/$1.png
 echo "slock.image: $WALLPAPERS/$1.png" > /var/tmp/wallpaper.Xresources && xrdb -merge /var/tmp/wallpaper.Xresources
 
 format-file.py $DOTFILES/rofi-colors.rasi.fmt \
-    --al=$(get-xresource.sh "background") \
-    --bg=$(get-xresource.sh "background") \
-    --bg1=$(get-xresource.sh "background1") \
-    --bg2=$(get-xresource.sh "background2") \
-    --bg3=$(get-xresource.sh "background3") \
-    --bg4=$(get-xresource.sh "background4") \
-    --fg=$(get-xresource.sh "foreground") \
+    --al=$background \
+    --bg=$background \
+    --bg1=$background1 \
+    --bg2=$background2 \
+    --bg3=$background3 \
+    --bg4=$background4 \
+    --fg=$foreground \
     --accent=$(get-xresource.sh "accent") \
     > $DOTFILES/rofi-colors.rasi
+
+format-file.py ~/.config/alacritty/alacritty.yml.fmt \
+    --bg=$background \
+    --fg=$foreground \
+    --red=$red \
+    --green=$green \
+    --yellow=$yellow \
+    --blue=$blue \
+    --magenta=$magenta \
+    --cyan=$cyan \
+    --gray=$gray \
+    > ~/.config/alacritty/alacritty.yml
 
 nohup start-dunst.sh &
 
